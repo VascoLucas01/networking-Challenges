aa29dbc5c3426e30c8c7ae338bd7abd1e544a465
vascolucas <vascolucas01@gmail.com>
2023-03-04 20:54:58 +0000

    Refactor script OpsChallenge08.py

--- Challenge08/OpsChallenge08.py
+++ Challenge08/OpsChallenge08.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+
+#Script : OpsChallenge08.sh
+#Purpose: Practice python skills
+#Why    : Understand how to work with list in pyhton
+
+### variables
+str = "aaa"
+list = []
+
+# fills the list with 10 positions
+for i in range(0,10):
+    list.insert(i,str)
+ 
+# print the fourth element of the list
+print(list[4])
+
+
+# assuming [ position 0 .. position 9 ]
+# print the last four elements
+print(list[6:10])
+
+# change the value of the seventh element to “onion”
+list[6] = "onion"
+
+print(list)
+
+
+
+ 
+
+
