1dcf530114559f265daac4a35e552cb351e8947c
vascolucas <vascolucas01@gmail.com>
2023-03-04 21:04:04 +0000

    Refactor script OpsChallenge08.py

--- Challenge08/OpsChallenge08.py
+++ Challenge08/OpsChallenge08.py
@@ -5,22 +5,27 @@
 #Why    : Understand how to work with list in pyhton
 
 ### variables
-str = "aaa"
+str = ["aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg", "hhh", "iii", "jjj"]
 list = []
 
-# fills the list with 10 positions
+# fill the list with 10 positions
+print("__________fill the list one by one__________")
 for i in range(0,10):
-    list.insert(i,str)
+    list.insert(i,str[i])
+    print(list)
  
 # print the fourth element of the list
-print(list[4])
+print("\nThe fourth element of the list: ")
+print("> ", list[4])
 
 
 # assuming [ position 0 .. position 9 ]
 # print the last four elements
-print(list[6:10])
+print("\nThe last four elements of the list:")
+print("> ", list[6:10])
 
 # change the value of the seventh element to “onion”
+print("\nChange the value of the seventh element to "onion":")
 list[6] = "onion"
 
 print(list)
