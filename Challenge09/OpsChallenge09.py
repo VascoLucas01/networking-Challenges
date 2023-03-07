ad3c36cc55ebb6d5a0789bdfbfa51eed7e4f887a
vascolucas <vascolucas01@gmail.com>
2023-03-07 17:41:58 +0000

    Refactor script OpsChallenge09.py

--- Challenge09/OpsChallenge09.py
+++ Challenge09/OpsChallenge09.py
@@ -5,22 +5,27 @@
 #Why    : Understand how to work with conditional statements
 
 threshold_value = 10
-input_value = ""
+is_numeric = False
 i = 0
 
-while(not input_value.isnumeric()):
-    if(i != 0):
-        print("Incorrect input value! Try again:")
-    input_value = input("_____________The threshold value is 10_____________\nEnter a different integer value: ")
-    
-    i = i + 1
+while( not is_numeric ):
 
-if(input_value > threshold_value):
-    print("É maior!")
-elif(input_value < threshold_value):
-    print("É menor!")
+    input_value = input("\n_____________The threshold value is 10_____________\n\nEnter a different integer value: ")
+    
+    if( input_value.isnumeric() ):
+        is_numeric = True
+        break
+    else:
+        print("\n--- > Incorrect input value! < --- Try again:")
+    
+
+
+if( int(input_value) > threshold_value ):
+    print("\n\t> É maior!")
+elif( int(input_value) < threshold_value ):
+    print("\n\t> É menor!")
 else:
-    print("É igual!")
+    print("\n\t> É igual!")
     
     
 
