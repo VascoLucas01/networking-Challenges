599e468427e74491867428e3548496b47ed99751
Vasco Coelho de Melo Gouveia Lucas <110473841+VascoLucas01@users.noreply.github.com>
2023-08-10 14:26:34 +0100

    Create Backup_Log_Messages.sh

---
+++ Backup_Log_Messages.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+#Script : Backup_Log_Messages.sh
+#Purpose: Make a backup of the log messages
+#Why    : To show some skills in file management
+
+
+filename="syslog.$(date +'[%d-%m-%Y_%H:%M:%S]')"
+
+
+# the script initial message
+echo -e "\n$(date +'[%Y-%m-%d T %H:%M:%S]') Script is starting...\n"
+
+
+# make a backup of the log messages to the filename
+journalctl --no-pager | cp /dev/stdin $filename
+
+# print the message that tells the success of the backup
+echo -e "$(date +'[%Y-%m-%d T %H:%M:%S]') The backup was a success!"
