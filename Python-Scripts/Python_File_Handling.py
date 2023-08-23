31d77a28deaed46e0c4e088cf39c066f47f69b4d
Vasco Coelho de Melo Gouveia Lucas <110473841+VascoLucas01@users.noreply.github.com>
2023-08-23 23:43:48 +0100

    Create Python_File_Handling.py

---
+++ Python-Scripts/Python_File_Handling.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+import subprocess
+
+#Script : Python_File_Handling.py
+#Purpose: Using file handling commands, create a new .txt file, appends three lines, prints to the screen the first line, then deletes the .txt file.
+#Why    : Understand how to work with files
+
+# creation of the fiel to write
+file = open("tmp.txt","a+")
+
+# write three lines
+file.write("First line\n")
+file.write("Second line\n")
+file.write("Third line\n")
+
+# the file pointer is not in at the beginning of the file 
+file.seek(0)
+
+# print the first line of the file
+first_line = file.readline()
+print("\n____________First Line____________")
+print(first_line)
+
+# close the file
+#file.close()
+
+
+### Stretch Goal ###
+file = open("config-pfSense.home.arpa-20230309233846.xml","r+")
+
+
+content = file.read()
+content = content.replace("Vasco Coelho de Melo Gouveia Lucas","Vasco Lucas")
+
+# the file pointer is pointing to the end of the file and this is not what we want
+file.seek(0)
+
+file.write(content)
+
+# because the file is shorter, it is necessary to remove any leftover data from the end of the file
+file.truncate()
+
+file.close()
+
+
+# removes the file
+subprocess.run(["rm","tmp.txt"],capture_output=True, text=True)
